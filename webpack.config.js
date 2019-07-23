861713dea8a2f8a008c6341ec4651f4cb6ee1b03
Joel <joelkaplan1@gmail.com>
2019-07-23 15:00:03 -0700

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const BrotliGzipPlugin = require('brotli-gzip-webpack-plugin');
+
+module.exports = {
+  entry: __dirname + '/client/src/index.jsx',
+  module: {
+    rules: [
+      { 
+        test: [/\.jsx$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/client/dist'
+  },
+  plugins: [
+    new BrotliGzipPlugin({
+        asset: '[path].br[query]',
+        algorithm: 'brotli',
+        test: /\.(js|css|html|svg)$/,
+        threshold: 10240,
+        minRatio: 0.8
+    }),
+    new BrotliGzipPlugin({
+        asset: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: /\.(js|css|html|svg)$/,
+        threshold: 10240,
+        minRatio: 0.8
+    })
+  ],
+};
