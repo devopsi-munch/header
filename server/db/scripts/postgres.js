80a478e53a1cec1c67849c638575ee7ceb55355f
Joel <joelkaplan1@gmail.com>
2019-07-26 15:03:20 -0700

    Modified schema and seed script

--- server/db/scripts/postgres.js
+++ server/db/scripts/postgres.js
@@ -12,30 +12,42 @@ const makeRestaurantName = () => {
   return `${adjective} ${foodTypes[Math.floor(Math.random() * foodTypes.length)]} ${foodPlaces[Math.floor(Math.random() * foodPlaces.length)]}`;
 };
 
-const listingDataGen = counter => `${counter},${makeRestaurantName()},true,${faker.random.number({ min: 1, max: 4 })}\n`;
-const reviewDataGen = (counter) => {
-  const month = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
-  const year = ['2015', '2016', '2017', '2018', '2019'];
-  
-}
+const listingDataGen = counter => `${counter},${makeRestaurantName()},${faker.random.boolean()},${faker.random.number({ min: 1, max: 4 })}\n`;
 
-const stream = fs.createWriteStream('postgresDataListing3.csv');
+const month = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+const year = ['2015', '2016', '2017', '2018', '2019']; 
+const categories = ['Pizza', 'Steak', 'Brunch', 'Seafood', 'Italian', 'Cafe', 'Restaurant', 'Shoppe', 'Diner'];
+
+const reviewDataGen = (reviewID, listingID) => {
+  return `${reviewID},${faker.random.number({ min: 1, max: 5 })},${year[faker.random.number({ min: 0, max: 4 })]}-${month[faker.random.number({min:0, max:11})]}-${faker.random.number({min: 1, max: 28}).toString()},${listingID}\n`;
+};
+
+const categoriesDataGen = counter => `${counter},${categories[faker.random.number({ min: 0, max: 8 })]},${counter}`;
+
+const stream = fs.createWriteStream('./data/categories2.csv');
+const rows = 10000;
 
 const dataGen = (writer, encoding, cb) => {
   let i = 0;
+  // let j = 0;
   write();
   function write() {
     let ok = true;
     do {
-      i++;
+      // let data = listingDataGen(i);
       // let data = `${i},${makeRestaurantName()},true,${faker.random.number({ min: 1, max: 4 })}\n`;
-      let data = listingDataGen(i);
-      if (i === 10000000) {
+      // let data = reviewDataGen(i, j);
+      let data = categoriesDataGen(i);
+      i++;
+      // if (i % 60 === 0) {
+      //   j++
+      // }
+      if (i === rows) {
         writer.write(data, encoding, cb);
       } else {
         ok = writer.write(data, encoding);
       }
-    } while (i < 10000000 && ok);
+    } while (i < rows && ok);
     if (i > 0) {
       writer.once('drain', write);
     }
@@ -43,28 +55,3 @@ const dataGen = (writer, encoding, cb) => {
 };
 
 dataGen(stream, 'utf8', () => {console.log('success'); });
-
-// const makeRow = (option, count) => {
-
-// }
-
-
-
-// counter = 0;
-// const reviewsDataGen = () => {
-//   writer.pipe(fs.createWriteStream('postgresReviews.csv'));
-//   for (var i = 0; i < 10000000; i++) {
-//     for (var j = 0; j < 60; j++) {
-//       writer.write({
-//         REVIEWID: counter++,
-//         RATING: 1,
-//         LISTINGID: i,
-
-//       })
-//     }
-//   }
-// }
-
-// listingDataGen();
-// reviewsDataGen();
-// categoriesDataGen();
