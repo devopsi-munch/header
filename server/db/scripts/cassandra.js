2f283f1c60c263cdf00ee4e7802002cae0c5aef1
Joel <joelkaplan1@gmail.com>
2019-07-26 19:16:25 -0700

    Modify postgres seed script to include other tables, modify cassandra schema to include clustering key

--- server/db/scripts/cassandra.js
+++ server/db/scripts/cassandra.js
@@ -1,8 +1,5 @@
 const fs = require('fs');
-// const csvWriter = require('csv-write-stream');
-// const writer = csvWriter();
 const faker = require('faker');
-// var counter = 0;
 
 const makeRestaurantName = () => {
   const foodTypes = ['Pizza', 'Steak', 'Brunch', 'Seafood', 'Italian', 'Chinese', 'Japanese', 'Korean', 'Seafood', 'Fish', 'Pho', 'Noodle', 'Ramen'];
@@ -12,7 +9,7 @@ const makeRestaurantName = () => {
   return `${adjective} ${foodTypes[Math.floor(Math.random() * foodTypes.length)]} ${foodPlaces[Math.floor(Math.random() * foodPlaces.length)]}`;
 };
 
-const listingDataGen = counter => `${counter},${makeRestaurantName()},${faker.random.boolean()},${faker.random.number({ min: 1, max: 4 })}\n`;
+const listingDataGen = counter => `${counter},${makeRestaurantName()},${faker.random.boolean()},${faker.random.number({ min: 1, max: 4 })}`;
 
 const month = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
 const year = ['2015', '2016', '2017', '2018', '2019']; 
@@ -24,24 +21,26 @@ const reviewDataGen = (reviewID, listingID) => {
 
 const categoriesDataGen = counter => `${counter},${categories[faker.random.number({ min: 0, max: 8 })]},${counter}\n`;
 
-const stream = fs.createWriteStream('./data/categories2.csv');
-const rows = 10000000;
+const stream = fs.createWriteStream('./data/all_listing_data.csv');
+const rows = 10000;
 
 const dataGen = (writer, encoding, cb) => {
   let i = 0;
-  // let j = 0;
+  let j = 0;
+  let restaurant = listingDataGen(j);
+  let category = categoriesDataGen(j);
   write();
   function write() {
     let ok = true;
-    do {
-      // let data = listingDataGen(i);
-      // let data = `${i},${makeRestaurantName()},true,${faker.random.number({ min: 1, max: 4 })}\n`;
+    do {   
       // let data = reviewDataGen(i, j);
-      let data = categoriesDataGen(i);
+      const data = `${restaurant},${reviewDataGen(j, i)},${category}`;
       i++;
-      // if (i % 60 === 0) {
-      //   j++
-      // }
+      if (i % 60 === 0) {
+        j++
+        restaurant = listingDataGen(i);
+        category = categoriesDataGen(i);
+      }
       if (i === rows) {
         writer.write(data, encoding, cb);
       } else {
